d34b827f2c6fb28f2d9ca88bc2b4c7f69487b083
Christine Feaster & Keaty <pair+femmestem.keaty@devbootcamp.com>
2014-04-25 18:11:19 -0400

    Created user authentication
    * users can create accounts
    * users can login
    * users can logout
    * users can only reach user page and profile when logged in

--- web_flash_cards/app/models/user.rb
+++ web_flash_cards/app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+
+  def self.authenticate(email, password)
+    user = User.where(["email = ?", email]).first
+    if user && user.password == password
+      user
+    else
+      nil
+    end
+  end
+
 end
