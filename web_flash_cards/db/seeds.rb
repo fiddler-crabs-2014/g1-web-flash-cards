a1575c3b0c77759c9aeea3f15f85793cd6247196
Timmy Huang <timmyhuang0404@gmail.com>
2014-04-25 20:45:30 -0400

    add spanish deck

--- web_flash_cards/db/seeds.rb
+++ web_flash_cards/db/seeds.rb
@@ -1,4 +1,14 @@
+@deck1 = Deck.create(topic: "french")
+@deck2 = Deck.create(topic: "spanish")
+
 File.open("english-french.txt").each do |line|
   q_a_pair = line.chomp.split("\t")
-  Card.create(question: q_a_pair[1], answer: q_a_pair[0])
+  card = Card.create(question: q_a_pair[1], answer: q_a_pair[0])
+  card.decks << @deck1
+end
+
+File.open("english-french.txt").each do |line|
+  q_a_pair = line.chomp.split("\t")
+  card = Card.create(question: q_a_pair[1], answer: q_a_pair[0])
+  card.decks << @deck2
 end
