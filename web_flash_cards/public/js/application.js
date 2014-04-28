ea062853aa5d4c65bf6b2baa1d591d8d4eb1bdb6
Keaty & Ryan Rebo <pair+keaty.reboryan@devbootcamp.com>
2014-04-27 20:24:45 -0400

    some css tweaks, especially to the answer display feature

--- web_flash_cards/public/js/application.js
+++ web_flash_cards/public/js/application.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  // on click from form on deck.erb
+  $('.check_answer').click(function(event){
+    event.preventDefault();//stop browser to take action for clicked anchor
+    $('.answer_card').removeClass('hide');
+    $('.card').addClass('hide');
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+    setTimeout(function() {
+      $('#guess').submit();
+   }, 1500);
+  });
 });
